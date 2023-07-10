8733cecb34e858d8cb10e9c5d3ac29f4f5d6531b
Gabriel <gabriel.fer.faria@gmail.com>
2023-07-10 18:42:56 -0400

    aula 5

--- ProjetoLivro/src/empresa/Principal.java
+++ ProjetoLivro/src/empresa/Principal.java
@@ -3,10 +3,23 @@ package empresa;
 public class Principal {
 	
 	public static void main(String[] args) {
-		LivroDigital ld= new LivroDigital("Senhor dos aneis",
+		LivroDigital livroA= new LivroDigital("Senhor dos aneis",
 				new Autor("Tolkien","Britanico","tolkien@email.com"),
 				"Aventura",5,10000,35000);
 		
-		ld.info();
+		livroA.info();
+		
+		LivroFisico livroB = new LivroFisico("Sherlock Holmes",
+				new Autor("Sir Arthur Conan Doyle","Britanico","arthur@email.com"),
+				"Aventura",2,50000,2500);
+		livroB.info();
+		System.out.println();
+		Livro livro;
+		
+		livro = livroA;
+		livro.info();
+		System.out.println();
+		livro=livroB;
+		livro.info();
 	}
 }
